b38d0e24ff4c7c789c8eb4acf21615e5f98c5a44
Eduardo Nunes <esnunes@gmail.com>
2016-05-19 14:48:31 -0300

    Remove branches command

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import "github.com/gpf-org/gpf/cmd"
 
 func main() {
 	cmd.RootCmd.AddCommand(cmd.ServerCmd)
-	cmd.RootCmd.AddCommand(cmd.BrahnchesCmd)
 	cmd.RootCmd.AddCommand(cmd.ReloadCmd)
 	cmd.RootCmd.AddCommand(cmd.ListCmd)
 	cmd.RootCmd.AddCommand(cmd.VersionCmd)
