086562b33c20a7e14ad843e87abdabb1231a9cd2
Eduardo Nunes <esnunes@gmail.com>
2016-05-19 21:22:27 -0300

    Remove unused flags

--- cmd/root.go
+++ cmd/root.go
@@ -6,26 +6,12 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var (
-	Provider string
-	Token    string
-	BaseURL  string
-)
 
 var RootCmd = &cobra.Command{
 	Use: "gpf",
-	PreRunE: func(cmd *cobra.Command, args []string) error {
-		if Token == "" {
-			return errors.New("missing required token flag")
-		}
-		return nil
-	},
 }
 
 func init() {
-	RootCmd.PersistentFlags().StringVarP(&Provider, "provider", "p", "", "Git provider (e.g. gitlab)")
-	RootCmd.PersistentFlags().StringVarP(&Token, "token", "t", "", "Private token")
-	RootCmd.PersistentFlags().StringVarP(&BaseURL, "baseURL", "b", "https://gitlab.com/api/v3", "Base URL")
 	RootCmd.AddCommand(ServerCmd)
 	RootCmd.AddCommand(ReloadCmd)
 	RootCmd.AddCommand(ListCmd)
